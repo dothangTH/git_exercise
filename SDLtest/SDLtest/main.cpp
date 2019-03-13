57690107054c0ed67ee2d6e8ee2c4e935b58fcc5
Thang <dothang.thanhhoa@gmail.com>
2019-03-13 09:31:29 +0700

    Modified SDLtest/SDLtest.

---
+++ SDLtest/SDLtest/main.cpp
@@ -0,0 +1,79 @@
+#include <SDL.h>
+#include <iostream>
+
+using namespace std;
+
+void logSDLError(std::ostream& os,
+                 const std::string &msg, bool fatal = false);
+
+void logSDLError(std::ostream& os,
+                 const std::string &msg, bool fatal)
+{
+    os << msg << " Error: " << SDL_GetError() << std::endl;
+    if (fatal) {
+        SDL_Quit();
+        exit(1);
+    }
+}
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const string WINDOW_TITLE = "An Implementation of Code.org Painter";
+
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        logSDLError(std::cout, "SDL_Init", true);
+
+    window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
+       SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
+       SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
+    if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
+
+
+    //Khi thông thường chạy với môi trường bình thường ở nhà
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+                                              SDL_RENDERER_PRESENTVSYNC);
+    //Khi chạy ở máy thực hành WinXP ở trường (máy ảo)
+    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
+    if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+}
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
+{
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+void waitUntilKeyPressed();
+
+void waitUntilKeyPressed()
+{
+    SDL_Event e;
+    while (true) {
+        if ( SDL_WaitEvent(&e) != 0 &&
+             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
+            return;
+        SDL_Delay(100);
+    }
+}int main(int argc, char* argv[])
+{
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    initSDL(window, renderer);
+
+    // Your drawing code here
+
+    // use SDL_RenderPresent(renderer) to show it
+
+    waitUntilKeyPressed();
+    quitSDL(window, renderer);
+    return 0;
+}
+
+
